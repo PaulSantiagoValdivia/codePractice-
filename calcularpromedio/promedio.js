0996ab98a4c145abdac856e384b9bd90f1918752
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-21 14:20:39 -0300

    promedio.js create function calculate mean and function is pair?

--- calcularpromedio/promedio.js
+++ calcularpromedio/promedio.js
@@ -1,5 +1,25 @@
-const salary = [230,300,400,500,320,402];
-const initialValue=0;
+const salary = [230,300,400,500,320,402,200];
+
+calculateMean(salary);
+function calculateMean(salary){
+    if(esPar(salary)){
+        console.log('cara de pija');
+    }else{
+    const meanOdd = Math.floor(salary.length /2);
+    console.log(meanOdd);
+    }
+}
+
+function esPar (salary){
+    let validate;
+    if(salary.length%2 === 0){
+        validate=true;
+    }else{
+        validate= false;
+    }
+    return validate;
+};
+
 const additionSalary = salary.reduce( (accumulator, courrentValue) => accumulator + courrentValue);
 const promedio = additionSalary / salary.length;
 console.log(promedio);
