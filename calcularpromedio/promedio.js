000a06c6419f1ce60a34cb06f2d50458de08652b
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-21 16:10:04 -0300

    promedio.js create function calculate the odd and even mean of a list sort

--- calcularpromedio/promedio.js
+++ calcularpromedio/promedio.js
@@ -1,35 +1,37 @@
-const salary = [230,300,400,500,320,402];
-
-calculateMean(salary);
-function calculateMean(salary){
-    if(esPar(salary)){
-        const indexMeanPair1 = (salary.length /2)-1;    
-        const indexMeanPair2 = salary.length /2;    
+const salary = [230, 300, 400, 1000, 500, 320];
+const additionSalary = salary.reduce((accumulator, courrentValue) => accumulator + courrentValue);
+const average = additionSalary / salary.length;
+console.log(average);
+calculateMean(salary);  
+function calculateMean(salary) {
+    const listSalary = salary.sort((accumulator, courrentValue) => accumulator - courrentValue);
+    if (esPar(listSalary)) {
+        const indexMeanPair1 = (listSalary.length / 2) - 1;
+        const indexMeanPair2 = listSalary.length / 2;
         let result;
-        console.log (indexMeanPair1); 
-        console.log (indexMeanPair2); 
-        result = (salary[indexMeanPair1] + salary[indexMeanPair2])/2;
-        console.log (result); 
-    }else{
-    const indexMeanOdd = Math.floor(salary.length /2);
-    const meandOdd = salary[indexMeanOdd];
-    console.log(indexMeanOdd);
-    console.log(meandOdd);
-    return meandOdd;
+        console.log(indexMeanPair1);
+        console.log(indexMeanPair2);
+        result = (listSalary[indexMeanPair1] + listSalary[indexMeanPair2]) / 2;
+        console.log(result);
+    } else {
+        const indexMeanOdd = Math.floor(listSalary.length / 2);
+        const meandOdd = listSalary[indexMeanOdd];
+        console.log(indexMeanOdd);
+        console.log(meandOdd);
+        return meandOdd;
     }
 }
 
-function esPar (salary){
+function esPar(listSalary) {
     let validate;
-    if(salary.length%2 === 0){
-        validate=true;
-    }else{
-        validate= false;
+    if (listSalary.length % 2 === 0) {
+        validate = true;
+    } else {
+        validate = false;
     }
     return validate;
 };
 
-    const additionSalary = salary.reduce( (accumulator, courrentValue) => accumulator + courrentValue);
-    const average = additionSalary / salary.length;
-    console.log(average);
+
+
 
