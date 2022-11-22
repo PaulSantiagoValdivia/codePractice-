4553ee0cdf3d5f4d89c94452a2bf8b4cd6c86a2d
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-22 19:53:24 -0300

    create function function to paint card

--- shoppigncart/shoppingcart.js
+++ shoppigncart/shoppingcart.js
@@ -1,11 +1,29 @@
-document.addEventListener('DOMContentLoaded'), =>{
-    fetchData;
-}
-const fetchData = async() => {
-    try{
+const items = document.getElementById('items');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+});
+const fetchData = async () => {
+    try {
         const res = await fetch('api.json');
-        const data = await.res.json();
-    }catch (error ){
-        console.log (error)
+        const data = await res.json();
+        pintarCards(data);
+    } catch (error) {
+        console.log(error)
     }
+}
+
+const pintarCards = data => {
+      data.forEach(producto => {
+        console.log(data);
+        templateCard.querySelector('h5').textContent = producto.title
+        templateCard.querySelector('P').textContent = producto.precio
+        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
 }
