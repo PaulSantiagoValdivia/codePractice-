e5f804f4421579bde9d764bd98041175b0ab5fb7
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-23 14:31:16 -0300

    paint from java the shopping cart model

--- shoppigncart/shoppingcart.js
+++ shoppigncart/shoppingcart.js
@@ -1,10 +1,14 @@
 const cards = document.getElementById('cards');
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
 const templateCard = document.getElementById('template-card').content;
+const templateFooter = document.getElementById('template-footer').content;
+const templateTrolley = document.getElementById('template-carrito').content;
 const fragment = document.createDocumentFragment();
-let shoppingCart ={}
+let shoppingCart ={};
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchData()
+    fetchData();
 });
 cards.addEventListener('click', e =>{
     addTrolley(e);
@@ -13,25 +17,25 @@ const fetchData = async () => {
     try {
         const res = await fetch('api.json');
         const data = await res.json();
-        pintarCards(data);
+        paintCards(data);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-const pintarCards = data => {
+const paintCards = data => {
       data.forEach(producto => {
 
-        templateCard.querySelector('h5').textContent = producto.title
-        templateCard.querySelector('P').textContent = producto.precio
-        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
-        templateCard.querySelector('.btn-dark').dataset.id = producto.id
+        templateCard.querySelector('h5').textContent = producto.title;
+        templateCard.querySelector('P').textContent = producto.precio;
+        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl);
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id;
 
-        const clone = templateCard.cloneNode(true)
-        fragment.appendChild(clone)
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
     })
 
-    cards.appendChild(fragment)
+    cards.appendChild(fragment);
 
 }
 const addTrolley = e => {
@@ -50,5 +54,20 @@ const setShoppingCart = objeto => {
     }
     shoppingCart.hasOwnProperty(product.id)? product.cantidad = shoppingCart[product.id].cantidad + 1 : false;
     shoppingCart[product.id] = {...product};
-    console.log(shoppingCart);
+    paintTrolley();
+}
+const paintTrolley = () =>{
+    items.innerHTML='';
+    Object.values(shoppingCart).forEach(product => {
+        templateTrolley.querySelector('th').textContent= product.id;
+        templateTrolley.querySelectorAll('td')[0].textContent = product.title; 
+        templateTrolley.querySelectorAll('td')[1].textContent = product.cantidad;
+        templateTrolley.querySelector('.btn-info').dataset.id =product.id;
+        templateTrolley.querySelector('.btn-danger').dataset.id =product.id;
+        templateTrolley.querySelector('span').textContent = product.cantidad * product.precio;
+
+        const clone = templateTrolley.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    items.appendChild(fragment)
 }
