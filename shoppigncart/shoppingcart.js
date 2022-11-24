3712338e55d737367e2ae03d8461fc1fadb6a0d3
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-23 21:32:36 -0300

    paint from java the paint footer  model,create functions for add up product quantities and add up all products

--- shoppigncart/shoppingcart.js
+++ shoppigncart/shoppingcart.js
@@ -5,12 +5,16 @@ const templateCard = document.getElementById('template-card').content;
 const templateFooter = document.getElementById('template-footer').content;
 const templateTrolley = document.getElementById('template-carrito').content;
 const fragment = document.createDocumentFragment();
-let shoppingCart ={};
+
+let shoppingCart = {};
 
 document.addEventListener('DOMContentLoaded', () => {
+
     fetchData();
+
 });
-cards.addEventListener('click', e =>{
+
+cards.addEventListener('click', e => {
     addTrolley(e);
 })
 const fetchData = async () => {
@@ -24,7 +28,7 @@ const fetchData = async () => {
 }
 
 const paintCards = data => {
-      data.forEach(producto => {
+    data.forEach(producto => {
 
         templateCard.querySelector('h5').textContent = producto.title;
         templateCard.querySelector('P').textContent = producto.precio;
@@ -40,34 +44,58 @@ const paintCards = data => {
 }
 const addTrolley = e => {
 
-    e.target.classList.contains('btn-dark') ? setShoppingCart(e.target.parentElement): false;
+    e.target.classList.contains('btn-dark') ? setShoppingCart(e.target.parentElement) : false;
     e.stopPropagation()
 
 }
 const setShoppingCart = objeto => {
+
     const product = {
+
         id: objeto.querySelector('.btn-dark').dataset.id,
         title: objeto.querySelector('h5').textContent,
         precio: objeto.querySelector('p').textContent,
-        cantidad : 1
-        
+        cantidad: 1
+
     }
-    shoppingCart.hasOwnProperty(product.id)? product.cantidad = shoppingCart[product.id].cantidad + 1 : false;
-    shoppingCart[product.id] = {...product};
+
+    shoppingCart.hasOwnProperty(product.id) ? product.cantidad = shoppingCart[product.id].cantidad + 1 : false;
+    shoppingCart[product.id] = { ...product };
     paintTrolley();
+
 }
-const paintTrolley = () =>{
-    items.innerHTML='';
+const paintTrolley = () => {
+    items.innerHTML = '';
+
     Object.values(shoppingCart).forEach(product => {
-        templateTrolley.querySelector('th').textContent= product.id;
-        templateTrolley.querySelectorAll('td')[0].textContent = product.title; 
+
+        templateTrolley.querySelector('th').textContent = product.id;
+        templateTrolley.querySelectorAll('td')[0].textContent = product.title;
         templateTrolley.querySelectorAll('td')[1].textContent = product.cantidad;
-        templateTrolley.querySelector('.btn-info').dataset.id =product.id;
-        templateTrolley.querySelector('.btn-danger').dataset.id =product.id;
+        templateTrolley.querySelector('.btn-info').dataset.id = product.id;
+        templateTrolley.querySelector('.btn-danger').dataset.id = product.id;
         templateTrolley.querySelector('span').textContent = product.cantidad * product.precio;
 
         const clone = templateTrolley.cloneNode(true);
         fragment.appendChild(clone);
-    })
-    items.appendChild(fragment)
+    });
+    items.appendChild(fragment);
+    paintFooter();
 }
+
+const paintFooter = () => {
+
+    footer.innerHTML = '';
+    Object.keys(shoppingCart).length === 0 ? footer.innerHTML = `<th scope="row" colspan="5">Carrito vacio - comience a Comprar</th>` : false;
+
+    const nAmount = Object.values(shoppingCart).reduce((acc, { cantidad }) => acc + cantidad, 0);
+    const nPrice = Object.values(shoppingCart).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0);
+    templateFooter.querySelectorAll('td')[0].textContent = nAmount;
+    templateFooter.querySelector('span').textContent = nPrice;
+
+    const clone = templateFooter.cloneNode(true);
+
+    fragment.appendChild(clone);
+    footer.appendChild(fragment);
+}
+
