16872101ddf173a3ce3433d33736dc28e0bcf15e
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-12-06 10:22:06 -0300

    create geters seters y objects

--- objetosPractice/index.js
+++ objetosPractice/index.js
@@ -1,45 +1,54 @@
-function Studen(name, years, approvedCourses) {
-    this.name = name;
-    this.years = years;
-    this.approvedCourses = approvedCourses;
-}
+import Course from './js/courses.js';
+import LearningPath from './js/learningpath.js';
+import Teachers from './js/teachers.js';
+import Studen from './js/studen.js';
 
-
-Studen.prototype.approvedCourses = function (newCourses) {
-    this.approvedCourses.push(newCourses);
-}
-
-const juanita = new Studen(
-    "juanita Aejandra",
-    16,
-    [
-        "curso de introduccion a la produccion de video juegos ",
-        "curso de creacion de personajes"
+const diegoDeGranda = new Teachers({
+    name:"Diego de granda",
+    username: "DiegoDg",
+    email: "diegodg@platzi.com",
+    yearsOfIntruction: 6,
+    coourses: []
+})
+const cursoJavaScript = new Course({
+    id: 'basico-javascript',
+    name: 'Curso Básico de JavaScript',
+    teacher: diegoDeGranda
+})
+const basicProgrammingCourse = new Course({
+    name: 'curso gratis de programacion basica',
+});
+const ecmaScript6 = new Course({
+    id: 'ecmascript-6',
+    name: 'Curso de ECMAScript 6+',
+    teacher: diegoDeGranda,
+})
+const reactNativeLearningPath = new LearningPath({
+    id: 'react-native',
+    name: 'Desarrollo de Apps con React Native',
+    courses: [
+        ecmaScript6,
+        cursoJavaScript,
+        basicProgrammingCourse
     ]
-)
+})
 
-class Studen2 {
-    constructor({
-        name = "",
-        approvedCourses = [],
-        years = 0,
-        email = ""
-    }) {
-        this.name = name;
-        this.years = years;
-        this.approvedCourses = approvedCourses;
-        this.email = email;
-    }
+const mike = new Studen({
+    name: "Mike",
+    email: "mike@platzi.com",
+    username: "Mikedc",
+    twitter: "Mikedc  ",
+    learningPath: [reactNativeLearningPath]
+});
 
-    passCourse(newCourses) {
-        this.approvedCourses.push(newCourses);
-    }
-}
-
-const mike = new Studen2({
-    years: 17,
-    name: "Miguel",
-    email: "miguel@platzi.com"
-}
-
-);
+const santiago = new Studen({
+    name: "santiago",
+    email: "santiago@platzi.com",
+    username: "santiagodc",
+    instagram: "santiagodc",
+    learningPath : [reactNativeLearningPath]
+});
+console.log(basicProgrammingCourse);
+basicProgrammingCourse.name = "curso malito de programacion basica";
+console.log(basicProgrammingCourse);
+console.log(basicProgrammingCourse);
---
+++ objetosPractice/js/studen.js
@@ -0,0 +1,27 @@
+export default class Studen {
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPath = []
+    }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        }
+        this.approvedCourses = approvedCourses;
+        this.learningPath = learningPath;
+    }
+
+    passCourse(newCourses) {
+        this.approvedCourses.push(newCourses);
+    }
+}
