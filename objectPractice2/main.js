941cd513eaf2098cb8fa0015c38ef47601df4f0b
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-12-08 18:20:25 -0300

    create function to make a copy of another object

--- objectPractice2/main.js
+++ objectPractice2/main.js
@@ -1,7 +1,46 @@
-function isObject (subject){
+const obj1 = {
+    a: 'a',
+    b: 'b',
+    c: 'c',
+    e: {
+        d: 'd',
+        e: 'e'
+    }
+}
+
+
+function isObject(subject) {
     return typeof subject == "object"
 }
 
-function isArray (subject){
+function isArray(subject) {
     return Array.isArray(subject);
+}
+
+function deepCopy(subject);
+let copySubject;
+const subjectIsObject = isObject(subject);
+const subjectIsArray = isArray(subject);
+if (subjectIsArray) {
+    copySubject = [];
+} else if (subjectIsObject) {
+    copySubject = {};
+} else {
+    return subject;
+}
+
+for (key in subject) {
+    const keyIsObject = isObject(subject[key])
+    if (keyIsObject) {
+        copySubject[key] = deepCopy(subject[key]);
+    } else {
+        if (subjectIsArray) {
+            copySubject.push(subject[key]);
+        } else {
+            copySubject[key] = subject[key]
+        }
+    }
+
+    return copySubject;
+
 }
